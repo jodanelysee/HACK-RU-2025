3b7c32841ee3d3bfa758afc6e44d7712d7c2d358
Jodan Elysee <jodanelysee@gmail.com>
2025-02-01 13:31:04 -0500

    connecting to mongodb

--- backend/index.js
+++ backend/index.js
@@ -19,27 +19,66 @@ app.get('/', (req, res) => {
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://user:pRBB2km5r7BKPur7@hackru.v2fpn.mongodb.net/?retryWrites=true&w=majority&appName=HackRU";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+    tls: true, // Enable TLS/SSL
+    serverSelectionTimeoutMS: 50000, // Increase server selection timeout
+    connectTimeoutMS: 40000, // Increase connection timeout
+  });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    //creating collection
+    const userCollection = client.db("UserInventory").collection("users");
+    const recipeCollection = client.db("recipeInventory").collection("recipes");
+
+    app.post("/register", async (req, res) => {
+        // check if email already exists
+        const data = req.body;
+        const existing = await userCollections.findOne({ email: data.email });
+        if (existing) {
+          res.status(400).send("Email already exists");
+          return;
+        }
+        const result = await userCollections.insertOne(data);
+        console.log(result)
+        const recipeResult = await recipeCollections.insertOne({recipes: [], user: result.insertedId})
+        res.send(result)
+      })
+
+      app.post("/login", async (req, res) => {
+        const { email, password } = req.body;
+        const user = await userCollections.findOne({ email, password });
+  
+        if (user) {
+          // User exists and credentials are correct
+          res.status(200).json({ success: true, message: "Login successful", user });
+        } else {
+          // User does not exist or credentials are incorrect
+          res.status(401).json({ success: false, message: "Incorrect email or password" });
+        }
+      });
+
+      app.get("/all-users", async (req, res) => {
+        const users = userCollections.find();
+        const result = await users.toArray();
+        res.send(result);
+      })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
+
 run().catch(console.dir);
 
 
