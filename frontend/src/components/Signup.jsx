4138522ec22ba390cd17d1187d4ac13584cb8371
Jodan Elysee <jodanelysee@gmail.com>
2025-02-02 11:15:54 -0500

    adding security on login and register page

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+ useEffect(() => {
+        localStorage.clear();
+      });
+
     const handleSignUp = (event) => {
         event.preventDefault();
         const form = event.target;
